20ce8c5925a6d1172bc2c574fb194154a9ffdc84
namhunKim <skagns211@gmail.com>
2022-03-01 01:32:21 +0900

    Docs: covid_nextjs

--- covid_nextjs/next.config.js
+++ covid_nextjs/next.config.js
@@ -1,6 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
+const API_KEY = process.env.API_KEY;
 
-module.exports = nextConfig;
+module.exports = {
+  reactStrictMode: true,
+
+  async rewrites() {
+    return [
+      {
+        source: "/api/covid",
+        destination: `http://apis.data.go.kr/B551182/pubReliefHospService/getpubReliefHospList?ServiceKey=juQIiU25rvu6W21we37f1D%252F2rdtOmzd8YI9tb06qPgf7cb1r%252BD2OlnqZmAyycI0%252BFrtnCuE5DtQpr3ZxTm%252FvlA%253D%253D&numOfRows=1000&spclAdmTyCd=A0`,
+      },
+    ];
+  },
+};
--- covid_nextjs/pages/index.js
+++ covid_nextjs/pages/index.js
@@ -1,17 +1,31 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const Home = () => {
-  // const url =
-  //   "?ServiceKey=juQIiU25rvu6W21we37f1D%2F2rdtOmzd8YI9tb06qPgf7cb1r%2BD2OlnqZmAyycI0%2BFrtnCuE5DtQpr3ZxTm%2FvlA%3D%3D&numOfRows=10&spclAdmTyCd=A0";
-  // useEffect(() => {
-  //   axios
-  //     .get(
-  //       "http://apis.data.go.kr/B551182/pubReliefHospService/getpubReliefHospList?ServiceKey=juQIiU25rvu6W21we37f1D%2F2rdtOmzd8YI9tb06qPgf7cb1r%2BD2OlnqZmAyycI0%2BFrtnCuE5DtQpr3ZxTm%2FvlA%3D%3D&numOfRows=10&spclAdmTyCd=A0"
-  //     )
-  //     .then((res) => console.log(res));
-  //   // .catch((err) => alert(err));
-  // });
+  const [data, setData] = useState();
+  useEffect(async () => {
+    await axios.get("api/covid").then((res) => {
+      const { item } = res.data.response.body.items;
+      setData(item);
+    });
+  }, []);
+  console.log(data);
+  let sido = data?.filter((el) => el.sgguNm === "화성시");
+  let wh = data && data.map((el) => el);
+  console.log(wh);
+
+  class Data {
+    constructor(sido, sggu, tel, name) {
+      this.sido = sido;
+      this.sggu = sggu;
+      this.tel = tel;
+      this.name = name;
+    }
+    getSido(sido) {
+      return sido;
+    }
+  }
+  let test = new Data();
 
   return (
     <div>
